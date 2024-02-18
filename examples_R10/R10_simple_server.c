9e09d9231f670193db026291af638a5bdef07bda
Dmitry Ilyin <dima@doty.ru>
2024-02-18 16:57:04 +0300

    Reformat source code and make suggested changes

--- examples_R10/R10_simple_server.c
+++ examples_R10/R10_simple_server.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <signal.h>
 #include <event2/buffer.h>
 #include <event2/event.h>
 #include <event2/http.h>
@@ -12,6 +14,13 @@ generic_request_handler(struct evhttp_request *req, void *ctx)
 	evbuffer_free(reply);
 }
 
+static void
+signal_cb(evutil_socket_t fd, short event, void *arg)
+{
+	printf("%s signal received\n", strsignal(fd));
+	event_base_loopbreak(arg);
+}
+
 int
 main()
 {
@@ -19,6 +28,7 @@ main()
 	char *http_addr = "0.0.0.0";
 	struct event_base *base;
 	struct evhttp *http_server;
+	struct event *sig_int;
 
 	base = event_base_new();
 
@@ -26,10 +36,14 @@ main()
 	evhttp_bind_socket(http_server, http_addr, http_port);
 	evhttp_set_gencb(http_server, generic_request_handler, NULL);
 
+	sig_int = evsignal_new(base, SIGINT, signal_cb, base);
+	event_add(sig_int, NULL);
+
 	printf("Listening requests on http://%s:%d\n", http_addr, http_port);
 
 	event_base_dispatch(base);
 
 	evhttp_free(http_server);
+	event_free(sig_int);
 	event_base_free(base);
 }
